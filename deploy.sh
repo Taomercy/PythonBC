835c229baf2cc6b3ac01421e2bfe11f92a45c2e4
EC2 Default User <ec2-user@ip-172-31-4-19.cn-north-1.compute.internal>
2021-12-22 06:35:37 +0000

    db

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 project_path=/home/ec2-user/PythonBC
 container_id=`docker ps -a | grep "pythonbc_web" | awk '{print $1}'`
-docker cp ${container_id}:/home/BCRUPro/db.sqlite3 ${HOME}
+#docker cp ${container_id}:/home/BCRUPro/db.sqlite3 ${HOME}
 
 pushd ${project_path}
     docker-compose down
     image_id=`docker images | grep "pythonbc_web" | awk '{print $3}'`
     docker rmi ${image_id}
     git reset --hard HEAD^ && git clean -xdf && git pull
-    mv ${HOME}/db.sqlite3 .
+    #mv ${HOME}/db.sqlite3 .
     docker-compose build && docker-compose up -d
 popd
 
